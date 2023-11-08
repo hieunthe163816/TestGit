5d1f54433f641c85b68f12e4471ff132ad552305
hieunthe163816 <hieunthe163816@fpt.edu.vn>
2023-11-08 20:10:53 +0700

    frist commit

--- TestGit/ViewController.swift
+++ TestGit/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("xin chao mọi người")
         // Do any additional setup after loading the view.
     }
 
