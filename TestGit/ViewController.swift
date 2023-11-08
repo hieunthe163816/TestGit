d5f7781c8dc0aad6680cdff08f3b131df4dd5c12
hieunthe163816 <hieunthe163816@fpt.edu.vn>
2023-11-08 20:07:06 +0700

    frist commit

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by Hiếu đang cáu on 08/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
